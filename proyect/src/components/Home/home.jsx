9f82da0f78359a20659ae7931b8b20137b4843d3
Lautaro Mansilla <85461971+laumansillaa@users.noreply.github.com>
2022-06-11 20:33:52 -0300

    add styles: media screen width 370px

--- proyect/src/components/Home/home.jsx
+++ proyect/src/components/Home/home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import photo from "../../assets/photo.png";
-import github from "../../assets/github.png";
-import gmail from "../../assets/gmail.png";
-import linkedin from "../../assets/linkedin.png";
-import whatsapp from "../../assets/whatsapp.png";
-import telegram from "../../assets/telegram.png";
+// import github from "../../assets/github.png";
+// import gmail from "../../assets/gmail.png";
+// import linkedin from "../../assets/linkedin.png";
+// import whatsapp from "../../assets/whatsapp.png";
+// import telegram from "../../assets/telegram.png";
 import About from '../About/About';
 import Contact from '../Contact/Contact'
 import Project from '../Project/Project';
@@ -44,13 +44,13 @@ const Home = () => {
                 <Contact />
             </div>
             <footer>
-                <div className='iconsFooter'>
+                {/* <div className='iconsFooter'>
                     <a href="https://www.github.com/laumansillaa" ><img src={github} alt='img github' title='Github'/></a>
                     <a href="mailto:mansillaalau@gmail.com" ><img src={gmail} alt='img gmail' title='Gmail'/></a>
                     <a href="https://www.linkedin.com/in/lautaro-mansilla" ><img src={linkedin} alt='img linkedin' title='Linkedin'/></a>
                     <a href="https://wa.me/542215062415" ><img src={whatsapp} alt='img whatsapp' title='WhatsApp'/></a>
                     <a href="https://t.me/Laumansillaa"><img src={telegram} alt='img telegram' title='Telegram'/></a>
-                </div>
+                </div> */}
                 <div className='copyright'>
                     <a >&copy; Lautaro Mansilla - FullStack Web Developer </a>
                 </div>
